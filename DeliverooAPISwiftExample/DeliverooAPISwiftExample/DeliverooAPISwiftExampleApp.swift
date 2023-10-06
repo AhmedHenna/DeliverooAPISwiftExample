6382be04d4c5ef7a8ca4f3a432a33782574b5e16
Ahmed Henna <85130496+AhmedHenna@users.noreply.github.com>
2023-10-06 20:34:02 +0300

    Added project

---
+++ DeliverooAPISwiftExample/DeliverooAPISwiftExample/DeliverooAPISwiftExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeliverooAPISwiftExampleApp.swift
+//  DeliverooAPISwiftExample
+//
+//  Created by Ahmed Henna on 10/6/23.
+//
+
+import SwiftUI
+
+@main
+struct DeliverooAPISwiftExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
